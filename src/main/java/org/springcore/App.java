745a371fbbb8a72a323f8045251fca1d81b2ff65
arpitthool <arpitthool1@gmail.com>
2022-06-15 19:18:18 -0400

    creating the Student class object using dependency injection.

--- src/main/java/org/springcore/App.java
+++ src/main/java/org/springcore/App.java
@@ -1,5 +1,8 @@
 package org.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        // dependency injections
+        // the Student object is created by the IOC container
+        // the IOC container manages the whole life cycle of the object, right from
+        // creation to destruction.
+
+        // we create the ApplicationContext by passing the file location where we
+        // have defined our beans
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        // we get the Student object from the context.
+        Student student1 = (Student) context.getBean("student1");
+
+        System.out.println(student1);
     }
 }
