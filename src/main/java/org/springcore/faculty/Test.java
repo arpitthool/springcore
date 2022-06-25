1fa8154b387fede3b73c9a9b839c2d6c415ed2fc
arpitthool <arpitthool1@gmail.com>
2022-06-25 13:06:05 -0400

    using AbstractApplicationContext instead of ApplicationContext to call the registerShutdownHook() to call destroy when bean is to be destroyed

--- src/main/java/org/springcore/faculty/Test.java
+++ src/main/java/org/springcore/faculty/Test.java
@@ -1,11 +1,12 @@
 package org.springcore.faculty;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("faculty/professor.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("faculty/professor.xml");
 
         Professor professor1 = (Professor) context.getBean("professor1");
         Professor professor2 = (Professor) context.getBean("professor2");
@@ -14,5 +15,7 @@ public class Test {
         System.out.println(professor1.toString());
         System.out.println(professor2.toString());
         System.out.println(professor3.toString());
+
+        context.registerShutdownHook();
     }
 }
