3a5fb675c7ad00b1148eb3e25f4ad521b38fe356
arpitthool <arpitthool1@gmail.com>
2022-06-25 19:31:57 -0400

    created methods to implement PostConstruct and PreDestroy annotations

--- src/main/java/org/springcore/faculty/TeachingAssistant.java
+++ src/main/java/org/springcore/faculty/TeachingAssistant.java
@@ -3,6 +3,9 @@ package org.springcore.faculty;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class TeachingAssistant implements InitializingBean, DisposableBean {
     private String name;
 
@@ -52,5 +55,15 @@ public class TeachingAssistant implements InitializingBean, DisposableBean {
 
     public void myDestroy() throws Exception {
         System.out.println("\nmyDestroy");
+    }
+
+    @PostConstruct
+    public void myInitAnnotation() throws Exception {
+        System.out.println("\nmyInitAnnotation()");
+    }
+
+    @PreDestroy
+    public void myDestroyAnnotation() throws Exception {
+        System.out.println("\nmyDestroyAnnotation()");
     }
 }
